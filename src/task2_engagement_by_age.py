85cd492b79b84e927c10639ae2df7d3921f8dce0
Ajitesh Reddy Tippireddy <ajiteshreddy7@gmail.com>
2025-09-19 12:07:41 -0400

    Update task2_engagement_by_age.py

--- src/task2_engagement_by_age.py
+++ src/task2_engagement_by_age.py
@@ -1,19 +1,1 @@
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import avg, col
 
-spark = SparkSession.builder.appName("EngagementByAgeGroup").getOrCreate()
-
-# Load datasets
-posts_df = spark.read.option("header", True).csv("input/posts.csv", inferSchema=True)
-users_df = spark.read.option("header", True).csv("input/users.csv", inferSchema=True)
-
-# TODO: Implement the task here
-engagement_df = (
-    posts_df.join(users_df, "UserID")
-    .groupBy("AgeGroup")
-    .agg(avg("Likes").alias("AvgLikes"), avg("Retweets").alias("AvgRetweets"))
-    .orderBy(col("AvgLikes").desc())
-)
-
-# Save result
-engagement_df.coalesce(1).write.mode("overwrite").csv("outputs/engagement_by_age.csv", header=True)
